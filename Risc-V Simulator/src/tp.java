04734418e3ac111dcaf7e12581392393edff569b
SRK2308 <kedarisamruddhi8@gmail.com>
2026-06-28 00:01:34 +0530

    Added the project

---
+++ Risc-V Simulator/src/tp.java
@@ -0,0 +1,9 @@
+public class tp {
+    
+    public static void main(String[] args)
+    {
+        String s = "x16";
+        int n = Integer.parseInt(s.substring(1));
+        System.out.println(n);
+    }
+}
